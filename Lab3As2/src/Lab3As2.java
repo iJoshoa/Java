7584608d8a06514d326a852691a046fbb73a7a49
iJoshoa <dfmn143@gmail.com>
2017-02-24 04:08:55 +0700

    24/2/2017

--- Lab3As2/src/Lab3As2.java
+++ Lab3As2/src/Lab3As2.java
@@ -13,17 +13,25 @@ public class Lab3As2 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Num = ");
         int num = sc.nextInt();
-        if (num%2==0 || num%3==0|| num%5==0 || num%7==0 || num%11==0) {
-            System.out.println("Number not Prime");
+        int a=0;       
+        for (int i = 2; i <= num; i++) {
+            for (int j=2; j <=i; j++) {
+                if(j==i){
+                    a=i;
+                }                   
+                if(i%j==0)break;                   
+            }
         }
-        else{
-            System.out.println("Number "+num+" is Prime");
+        if(a==num){
             for (int i = 2; i <= num; i++) {
                 for (int j=2; j <=i; j++) {
                     if(j==i) System.out.print(i+" ");                   
                     if(i%j==0)break;                   
                 }
             }
+        }
+        else{
+            System.out.println("Number not Prime");
         }
     }
 }
---
+++ mod/src/mod.java
@@ -0,0 +1,12 @@
+
+
+/**
+ *
+ * @author $_F
+ */
+public class mod {
+    public static void main(String[] args) {
+        System.out.println(7%3);
+    }
+    
+}
